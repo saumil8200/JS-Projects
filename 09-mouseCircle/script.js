790fd4e04dc4164988fcba4fbbe29115fce94fad
saumil8200 <saumil8200@gmail.com>
2024-01-07 14:51:28 +0530

    09-mouseCircle

---
+++ 09-mouseCircle/script.js
@@ -0,0 +1,22 @@
+const cursor = document.querySelector('.cursor');
+const colors = [
+  '#FF6633',
+  '#FFB399',
+  '#FF33FF',
+  '#FFFF99',
+  '#00B3E6',
+  '#E6B333',
+  '#3366E6',
+  '#999966',
+  '#99FF99',
+  '#B34D4D',
+];
+
+document.addEventListener('mousemove', (e) => {
+  cursor.style.left = e.pageX + 'px';
+  cursor.style.top = e.pageY + 'px';
+
+  const colorIndex = Math.floor(Math.random() * colors.length);
+  const randomColor = colors[colorIndex];
+  cursor.style.backgroundColor = randomColor;
+});

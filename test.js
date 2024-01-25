faa9a9e6df7ad7eefbd8141c7d2ee9dfda2fe4ab
saumil8200 <saumil8200@gmail.com>
2024-01-25 14:02:15 +0530

    05-keyboard

--- test.js
+++ test.js
@@ -1,15 +1,36 @@
 // HOISTING In JavaScript
-getName();
-console.log(x);
+// getName();
+// console.log(x);
 
-console.log(getName);
+// console.log(getName);
 
-var x = 7;
-function getName() {
-    console.log("HELLO");
+// var x = 7;
+// function getName() {
+//     console.log("HELLO");
+// }
+
+// getName();
+// console.log(x);
+
+// console.log(getName);
+
+// let a = 100;
+// {
+//     let a = 10;
+//     let b = 20;
+//     const c = 30;
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+
+// console.log(a);
+
+function x() {
+    var a = 7;
+    function y() {
+        console.log(a);
+    }
+    y();
 }
-
-getName();
-console.log(x);
-
-console.log(getName);
+x();

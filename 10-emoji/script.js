4deab423317d2db709185a5d7f46ac982c566bd1
saumil8200 <saumil8200@gmail.com>
2024-01-25 12:17:31 +0530

    10-emoji

---
+++ 10-emoji/script.js
@@ -0,0 +1,48 @@
+const btn = document.querySelector('#emoji');
+const emojis = [
+  '😆',
+  '😅',
+  '🤣',
+  '😂',
+  '😀',
+  '🤑',
+  '🤨',
+  '🙂',
+  '😊',
+  '😗',
+  '😛',
+  '😏',
+  '🤥',
+  '😴',
+  '🥺',
+  '😧',
+  '😇',
+  '😳',
+  '🙃',
+  '🥴',
+  '🧐',
+  '🤨',
+  '😒',
+  '🤔',
+  '🤭',
+  '🥰',
+  '🤐',
+  '👀',
+  '🤔',
+  '🤪',
+  '🥲',
+  '😃',
+  '😁',
+  '😬',
+];
+
+function getRandomEmoji(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
+
+btn.addEventListener('mouseover', function () {
+  const randomEmoji = getRandomEmoji(emojis);
+  console.log(randomEmoji);
+  btn.textContent = randomEmoji;
+});

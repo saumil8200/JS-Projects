dddad738eb3ebb048fb599907174ff964df332ee
saumil8200 <saumil8200@gmail.com>
2024-01-31 09:41:59 +0530

    generates color through code

--- 06-unlimitedColors/script.js
+++ 06-unlimitedColors/script.js
@@ -1,18 +1,25 @@
-const colors = ['red', 'blue', 'green', 'yellow', 'orange', 'purple'];
+// const colors = ['red', 'blue', 'green', 'yellow', 'orange', 'purple'];
 const startBtn = document.getElementById('start');
 const stopBtn = document.getElementById('stop');
 let startInterval;
 let index = 0;
 
+const colors = [];
+for (let i = 0; i < 25; i++) {
+  const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  colors.push(randomColor);
+}
+console.log(colors);
+
 startBtn.addEventListener('click', () => {
   startInterval = setInterval(function () {
-    console.log('START');
+    // console.log('START');
     document.body.style.backgroundColor = colors[index];
     index = (index + 1) % colors.length;
   }, 1000);
 });
 
 stopBtn.addEventListener('click', () => {
-  console.log('STOP');
+  // console.log('STOP');
   clearInterval(startInterval);
 });
